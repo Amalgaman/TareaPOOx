295a8a460ba19c9d19423e1cfda0129731a749c4
escuela@davinci.edu.ar <xxxrigobertoxxx@gmail.com>
2022-11-22 17:13:01 -0300

    submit 2

--- Modelo_Parcial_2/src/Interfaz/Main.java
+++ Modelo_Parcial_2/src/Interfaz/Main.java
@@ -9,6 +9,13 @@ class Main {
 	public static void main(String[] args) {
 		LinkedList<Colectivo> flota = new LinkedList<Colectivo>();
 
+		flota.add(new Colectivo("12", 1, 3, false, 5,new Chofer(1, "Mario")));
+		flota.add(new Colectivo("102", 2, 4, false, 8,new Chofer(2, "Tito")));
+		flota.add(new Colectivo("120", 3, 5, false, 6,new Chofer(3, "Pepe")));
+		
+		String[] paradas = {"Constitucion","Independencia","Corrientes","Cordoba","Retiro"}; 
+		
+		
 	}
 
 }
--- Modelo_Parcial_2/src/Logica/Pasajero.java
+++ Modelo_Parcial_2/src/Logica/Pasajero.java
@@ -2,4 +2,55 @@ package Logica;
 
 public class Pasajero {
 
+	private static int id=1;
+	private String nombre;
+	private double saldo;
+	private int edad;
+	private int paradas_recorridas;
+	
+	public Pasajero(int id, String nombre, double saldo, int edad) {
+		super();
+		this.id = id;
+		this.nombre = nombre;
+		this.saldo = saldo;
+		this.edad = edad;
+		this.paradas_recorridas = 0;
+		this.id++;
+	}
+
+	public static int getId() {
+		return id;
+	}
+
+	public static void setId(int id) {
+		Pasajero.id = id;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getSaldo() {
+		return saldo;
+	}
+
+	public void setSaldo(double saldo) {
+		this.saldo = saldo;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+	
+	
+	
+	
 }
